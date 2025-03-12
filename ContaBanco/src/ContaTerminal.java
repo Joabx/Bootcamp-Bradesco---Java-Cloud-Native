af8e9d7170086e1b10137efb5c7f0687af3545bd
Joabx <joabfwf@hotmail.com>
2025-03-12 11:27:35 -0300

    primeiro commit de teste

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bom dia!!!");
+
+        System.out.println("Digite o número da conta: ");
+
+        scanner.nextLine(); 
+
+        
+
+
+
+    }
+
+
+
+
+
+}
